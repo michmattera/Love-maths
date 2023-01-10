7f96f5de6860a3e2086a6a2af5bd82516cb360e1
michmattera <michellemattera97@gmail.com>
2023-01-10 10:37:35 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,30 @@ document.addEventListener("DOMContentLoaded" , function(){
                 alert('You clicked submit!');
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game "loop" when the first page is loaded
  * and after the main answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
+
+    // Two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}.Aborting!`;
+    }
 
 }
 
@@ -39,7 +50,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
