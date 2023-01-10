69dd08cb6fc6e574228f5539ff7de2193555cdfd
michmattera <michellemattera97@gmail.com>
2023-01-10 10:22:15 +0000

    Add random numbers for user question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,8 +13,13 @@ document.addEventListener("DOMContentLoaded" , function(){
     }
 })
 
-
+/**
+ * The main game "loop" when the first page is loaded
+ * and after the main answer has been processed
+ */
 function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
